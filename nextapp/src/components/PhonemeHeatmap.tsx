10d6f96db090c8d83a155c9790280e24fe0e2154
markd05 <manassasmark@gmail.com>
2025-09-27 14:42:39 -0400

    table components

--- nextapp/src/components/PhonemeHeatmap.tsx
+++ nextapp/src/components/PhonemeHeatmap.tsx
@@ -41,7 +41,7 @@ export default function PhonemeHeatmap({ phonemeScores }: HeatmapProps) {
         yaxis: { visible: false, fixedrange: true },
 		
 		}}
-      style={{ width: "100%", height: "400px" }}
+      style={{ width: "100%", height: "auto" }}
     />
   );
 }
