5cda6b05c671c6ab2698ae379ee3c13677acc4c0
markd05 <manassasmark@gmail.com>
2025-09-27 01:23:34 -0400

    added phemone heatmap component

---
+++ nextapp/src/components/PhonemeHeatmap.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import React from "react";
+import Plot from "react-plotly.js";
+
+interface HeatmapProps {
+  phonemeScores: { [key: string]: number };
+}
+
+export default function PhonemeHeatmap({ phonemeScores }: HeatmapProps) {
+  const phonemes = Object.keys(phonemeScores);
+  const scores = Object.values(phonemeScores);
+
+  return (
+    <Plot
+      data={[{
+        z: [scores],
+        x: phonemes,
+        y: [""],
+        type: "heatmap",
+        colorscale: "RdYlGn",
+        zmin: 0,
+        zmax: 100,
+      }]}
+      layout={{ title: { text: "Phonetic Profile Heatmap" }}}
+      style={{ width: "100%", height: "400px" }}
+    />
+  );
+}
